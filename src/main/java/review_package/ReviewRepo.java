c3b2dbdcd5538d651f269ded0d5b8c860ba991fd
Micaela <msberry9@gmail.com>
2017-06-25 16:09:15 -0400

    added extra links

--- src/main/java/review_package/ReviewRepo.java
+++ src/main/java/review_package/ReviewRepo.java
@@ -17,15 +17,25 @@ public class ReviewRepo {
 
 	public ReviewRepo() {
 		TacoReview condadoTacos = new TacoReview(1, "Condado", "images/condadoTacos.jpeg",
-				"9 out of 10 tacos for taste", "reviews of tacos");
+				"4 out of 5 Tacos",
+				"Condado Tacos... This establishment seems to be the new hip place to visit for DIY tacos. "
+				+ "They have a simple form that you fill out specifying all the delicious tacos toppings you can think of. "
+				+ "Fear not vegetarian/vegan friends, they also have options for you if you aren't feeling carnivorius. "
+				+ "Keep in mind that the usual free chips and salsa you get at restaurants is not free at all. Still delicious!");
 		reviews.put(1L, condadoTacos);
 
 		TacoReview cantinaTacos = new TacoReview(2, "Local Cantina", "images/condadoTacos.jpeg",
-				"7 out of 10 tacos for taste", "reviews of tacos");
+				"3 out of 5 Tacos", "Local Cantina... This fun taco joint has everything you are looking for if a fun night on the "
+						+ "town is #goals. Tacos, Booze, Parties? Can you ask for more? Oh yeah... free chips and a variety of salsa"
+						+ " to choose from. Their tacos are tasty but they fall short on filling it to the brim. But $2 Taco Tuesday "
+						+ "makes up for that.");
 		reviews.put(2L, cantinaTacos);
 
-		TacoReview mimexico = new TacoReview(3, "Mi Mexico", "images/condadoTacos.jpeg", "8 out of 10 tacos for taste",
-				"reviews of tacos");
+		TacoReview mimexico = new TacoReview(3, "Mi Mexico", "images/condadoTacos.jpeg", "4.5 out of 5 Tacos",
+				"Mi Mexico... This restaurant is a bit outside downtown Columbus but worth the drive. If you are looking for an "
+				+ "affordable place to eat authentic Mexican food look no further. They have a huge menu to pick from but that "
+				+ "doesn't stop them from still making sure their tacos are top of the line. Like your food extra hot? You may "
+				+ "think you know what extra hot means until you visit.");
 		reviews.put(3L, mimexico);
 
 	}
